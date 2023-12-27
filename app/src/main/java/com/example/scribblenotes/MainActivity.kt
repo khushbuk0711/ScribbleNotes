d9d5a1d68467583c58147474ac21d3f9bab9b28b
khush <khushbuk0711@gmail.com>
2023-12-28 02:20:07 +0530

    commit

--- app/src/main/java/com/example/scribblenotes/MainActivity.kt
+++ app/src/main/java/com/example/scribblenotes/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.scribblenotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toolbar
 import com.example.scribblenotes.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +25,16 @@ class MainActivity : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.options, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        firebaseAuth = FirebaseAuth.getInstance()
+        val itemId = item.itemId
+        if(itemId== R.id.logout){
+            firebaseAuth.signOut()
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
